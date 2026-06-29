01e87d3e710049d88ff799e5e75f6926ac4c3856
min <wmp1f23@soton.ac.uk>
2026-06-29 20:06:08 +0800

    Done 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...restArgs) {
+  
+    for (const arg of restArgs){
+        if (!arr.includes(arg)){
+            continue;
+        }
+        let firstIndex = arr.indexOf(arg);
+        while(firstIndex !== arr.lastIndexOf(arg)){
+            arr.splice(arr.indexOf(arg), 1);
+        }
+        arr.splice(arr.indexOf(arg), 1);
+    }
+    return arr; 
+    
 };
 
 // Do not edit below this line
