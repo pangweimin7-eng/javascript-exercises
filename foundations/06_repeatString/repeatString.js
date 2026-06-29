250cb8de4d9e81a3e3fdf3571bac494490178d33
min <wmp1f23@soton.ac.uk>
2026-06-29 18:14:40 +0800

    Done 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let res = ""; 
+    if (num < 0){
+        return "ERROR";
+    }
+    while (num > 0){
+        res += str;
+        num--;
+    }
+    return res;
 };
 
 // Do not edit below this line
