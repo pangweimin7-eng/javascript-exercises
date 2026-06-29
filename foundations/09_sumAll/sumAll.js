97f077898874bf9708a0e0e78e5f54063243a1d4
min <wmp1f23@soton.ac.uk>
2026-06-29 20:52:56 +0800

    Done 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    args.sort((a,b) => a -b);
+    if (args[0] < 0 || !Number.isInteger(args[0]) || !Number.isInteger(args[1])){
+        return "ERROR";
+    }
+    let a = args[0];
+    let b = args[1];
+    let sum = 0;
+    while (a <= b){
+        sum += a;
+        a++;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
